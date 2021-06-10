6b6c27792396592d1607078f5a2b7c2fb8ed3c0b
Matthew Boeving <mboeving1@gmail.com>
2021-06-09 23:29:52 -0400

    Loops ex1 submission

---
+++ script.js
@@ -0,0 +1,35 @@
+for (i=1; i<=10; i++){
+    console.log(i);
+}
+for (i=10; i>=1; i--){
+    console.log(i);
+}
+
+let x = 0;
+while(x<10){
+    x++;
+    console.log(x);
+}
+
+let y = 11;
+while(1<y){
+    y--;
+    console.log(y);
+}
+
+let z = 0;
+do{
+    z++;
+    console.log(z);
+} while(z<10);
+
+let w = 11;
+do{
+    w--;
+    console.log(w);
+}while(1<w);
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+for(let i = 0; i < numbers.length; i++ ){
+    console.log(numbers[i])
+}
